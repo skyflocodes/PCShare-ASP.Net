42960a1f9c3bd7bae10b2a46ce850afef6de5173
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2020-11-27 17:00:28 -0500

    files added
    added test method and class

---
+++ PCShare Test/UnitTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PCShare_Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
