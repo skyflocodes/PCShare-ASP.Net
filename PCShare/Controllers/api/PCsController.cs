8fc1fc291536fce5989612b94ebad75f5aa7aa41
Sky Florence <55286296+skyflocodes@users.noreply.github.com>
2020-12-06 20:11:53 -0500

    PC api controller added
    api for CRUD on PC is added

---
+++ PCShare/Controllers/api/PCsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PCShare.Data;
+using PCShare.Models;
+
+namespace PCShare.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PCsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PCsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PCs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PC>>> GetPC()
+        {
+            return await _context.PC.ToListAsync();
+        }
+
+        // GET: api/PCs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PC>> GetPC(int id)
+        {
+            var pC = await _context.PC.FindAsync(id);
+
+            if (pC == null)
+            {
+                return NotFound();
+            }
+
+            return pC;
+        }
+
+        // PUT: api/PCs/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPC(int id, PC pC)
+        {
+            if (id != pC.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pC).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PCExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PCs
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<PC>> PostPC(PC pC)
+        {
+            _context.PC.Add(pC);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPC", new { id = pC.Id }, pC);
+        }
+
+        // DELETE: api/PCs/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<PC>> DeletePC(int id)
+        {
+            var pC = await _context.PC.FindAsync(id);
+            if (pC == null)
+            {
+                return NotFound();
+            }
+
+            _context.PC.Remove(pC);
+            await _context.SaveChangesAsync();
+
+            return pC;
+        }
+
+        private bool PCExists(int id)
+        {
+            return _context.PC.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ PCShare/Data/Migrations/20201207005044_migration 23.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PCShare.Data.Migrations
+{
+    public partial class migration23 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Pcs",
+                table: "Pcs");
+
+            migrationBuilder.RenameTable(
+                name: "Pcs",
+                newName: "PC");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Pcs_UserId",
+                table: "PC",
+                newName: "IX_PC_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_PC",
+                table: "PC",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_PC",
+                table: "PC");
+
+            migrationBuilder.RenameTable(
+                name: "PC",
+                newName: "Pcs");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PC_UserId",
+                table: "Pcs",
+                newName: "IX_Pcs_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Pcs",
+                table: "Pcs",
+                column: "Id");
+        }
+    }
+}
